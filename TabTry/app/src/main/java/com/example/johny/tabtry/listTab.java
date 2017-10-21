f0e7e522fa5c28d5a7a2f04b2112ab1687342823
jeandeb <johnydebod@gmail.com>
2017-10-21 16:44:31 +0800

    New Tabs Menu

---
+++ TabTry/app/src/main/java/com/example/johny/tabtry/listTab.java
@@ -0,0 +1,21 @@
+package com.example.johny.tabtry;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by johny on 21/10/2017.
+ */
+
+public class listTab extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        View rootView = inflater.inflate(R.layout.list_tab, container, false);
+        return rootView;
+    }
+}
---
+++ TabTry/app/src/main/java/com/example/johny/tabtry/userTab.java
@@ -0,0 +1,19 @@
+package com.example.johny.tabtry;
+
+/**
+ * Created by johny on 21/10/2017.
+ */
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class userTab extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.user_tab, container, false);
+        return rootView;
+    }
+}
