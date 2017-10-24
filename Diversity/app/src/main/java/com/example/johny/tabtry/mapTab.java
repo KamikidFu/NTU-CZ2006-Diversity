1b35f0b4daad634558edc020324a340269d98194
Kamikid <kidfu@live.com>
2017-10-24 18:56:03 +0800

    Add a map in mapTry

--- Diversity/app/src/main/java/com/example/johny/tabtry/mapTab.java
+++ Diversity/app/src/main/java/com/example/johny/tabtry/mapTab.java
@@ -1,21 +1,54 @@
 package com.example.johny.tabtry;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 /**
  * Created by johny on 21/10/2017.
  */
 
-public class mapTab extends Fragment {
+public class mapTab extends Fragment implements OnMapReadyCallback {
+    private GoogleMap mMap;
+    private MapView mMapView;
+    private View mView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.map_tab, container, false);
-        return rootView;
+        mView = inflater.inflate(R.layout.map_tab, container, false);
+        return mView;
+    }
+
+    @Override
+    public void onViewCreated(View view,  Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mMapView = (MapView) mView.findViewById(R.id.mapView);
+        if(mMapView!=null){
+            mMapView.onCreate(null);
+            mMapView.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        MapsInitializer.initialize(getContext());
+        mMap = googleMap;
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(1.352067, 103.685649)).title("Hall 9").snippet("Where I live"));
+        CameraPosition HallNine = CameraPosition.builder().target(new LatLng(1.352067, 103.685649)).zoom(16).bearing(0).tilt(45).build();
+        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(HallNine));
     }
 }
